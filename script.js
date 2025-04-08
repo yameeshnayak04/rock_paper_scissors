24a0a667cf8556d1050a301f17114307c74a00d2
Yameesh Nayak <161949346+yameeshnayak04@users.noreply.github.com>
2025-04-08 09:47:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,6 @@ const playGame = (userChoice) => {
 choices.forEach((choice) =>{
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
-        console.log("choice was clicked" , userChoice);
         playGame(userChoice);
     });
 });
